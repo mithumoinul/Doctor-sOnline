2a9a883853c25e50096672761a6fa932ac990374
moinulmithu <moinulmithu@hotmail.com>
2015-02-14 21:32:39 +0600

    New theme

--- DoctorsOnline/App_Start/BundleConfig.cs
+++ DoctorsOnline/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace DoctorsOnline
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/amelia.bootstrap.css",
                       "~/Content/site.css"));
         }
     }
