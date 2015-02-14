6b10fcd1bb2327ab290d7a5cfbaea363d33cb7d2
moinulmithu <moinulmithu@hotmail.com>
2015-02-14 19:25:47 +0600

    Doctors Info without search and sort

--- DoctorsOnline/Controllers/DoctorsInfoesController.cs
+++ DoctorsOnline/Controllers/DoctorsInfoesController.cs
@@ -6,8 +6,8 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using DoctorsOnline.Models;
 using Model;
+using DoctorsOnline.Models;
 
 namespace DoctorsOnline.Controllers
 {
@@ -15,141 +15,112 @@ namespace DoctorsOnline.Controllers
     {
         private DoctorsOnlineContext db = new DoctorsOnlineContext();
 
-        // GET: DoctorsInfoes
-        public ActionResult Index(string sortOrder, string searchString)
+        // GET: /DoctorsInfoes/
+        public ActionResult Index()
         {
-            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
-            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
-            ViewBag.SpecialistSortParm = sortOrder == "Specialist" ? "spe_des" : "Specialist";
-
-            var doctors = from s in db.DoctorsInfos
-                          select s;
-
-            if (!String.IsNullOrEmpty(searchString))
-            {
-
-                doctors = doctors.Where(s => s.DoctorName.ToUpper().Contains(searchString.ToUpper()));
-            }
-
-            switch (sortOrder)
-            {
-                case "name_desc": doctors.OrderByDescending(s => s.DoctorName);
-                    break;
-
-                case "Date": doctors.OrderBy(s => s.VisitStartTime);
-                    break;
-
-                case "date_desc": doctors.OrderByDescending(s => s.VisitStartTime);
-                    break;
-
-                case "Specialist": doctors.OrderBy(s => s.Specialist);
-                    break;
-
-                case "spe_des": doctors.OrderByDescending(s => s.Specialist);
-                    break;
-
-                default:
-                    doctors.OrderBy(s => s.DoctorName);
-                    break;
-            }
-            return View(doctors.ToList());
-
-           // return View(db.DoctorsInfos.ToList());
+            var doctorsinfos = db.DoctorsInfos.Include(d => d.Department).Include(d => d.Hospital);
+            return View(doctorsinfos.ToList());
         }
 
-        // GET: DoctorsInfoes/Details/5
+        // GET: /DoctorsInfoes/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            DoctorsInfo doctorsInfo = db.DoctorsInfos.Find(id);
-            if (doctorsInfo == null)
+            DoctorsInfo doctorsinfo = db.DoctorsInfos.Find(id);
+            if (doctorsinfo == null)
             {
                 return HttpNotFound();
             }
-            return View(doctorsInfo);
+            return View(doctorsinfo);
         }
 
-        // GET: DoctorsInfoes/Create
+        // GET: /DoctorsInfoes/Create
         public ActionResult Create()
         {
+            ViewBag.DepartmentId = new SelectList(db.Departments, "Id", "DepartmentName");
+            ViewBag.HospitalId = new SelectList(db.Hospitals, "Id", "HospitalName");
             return View();
         }
 
-        // POST: DoctorsInfoes/Create
+        // POST: /DoctorsInfoes/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,DoctorName,Qualification,Specialist,Gender,VisitStartTime,VisitEndTime")] DoctorsInfo doctorsInfo)
+        public ActionResult Create([Bind(Include="Id,DoctorName,Qualification,Specialist,Gender,VisitStartTime,VisitEndTime,HospitalId,DepartmentId")] DoctorsInfo doctorsinfo)
         {
             if (ModelState.IsValid)
             {
-                db.DoctorsInfos.Add(doctorsInfo);
+                db.DoctorsInfos.Add(doctorsinfo);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(doctorsInfo);
+            ViewBag.DepartmentId = new SelectList(db.Departments, "Id", "DepartmentName", doctorsinfo.DepartmentId);
+            ViewBag.HospitalId = new SelectList(db.Hospitals, "Id", "HospitalName", doctorsinfo.HospitalId);
+            return View(doctorsinfo);
         }
 
-        // GET: DoctorsInfoes/Edit/5
-        [Authorize]
+        // GET: /DoctorsInfoes/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            DoctorsInfo doctorsInfo = db.DoctorsInfos.Find(id);
-            if (doctorsInfo == null)
+            DoctorsInfo doctorsinfo = db.DoctorsInfos.Find(id);
+            if (doctorsinfo == null)
             {
                 return HttpNotFound();
             }
-            return View(doctorsInfo);
+            ViewBag.DepartmentId = new SelectList(db.Departments, "Id", "DepartmentName", doctorsinfo.DepartmentId);
+            ViewBag.HospitalId = new SelectList(db.Hospitals, "Id", "HospitalName", doctorsinfo.HospitalId);
+            return View(doctorsinfo);
         }
 
-        // POST: DoctorsInfoes/Edit/5
+        // POST: /DoctorsInfoes/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,DoctorName,Qualification,Specialist,Gender,VisitingTime")] DoctorsInfo doctorsInfo)
+        public ActionResult Edit([Bind(Include="Id,DoctorName,Qualification,Specialist,Gender,VisitStartTime,VisitEndTime,HospitalId,DepartmentId")] DoctorsInfo doctorsinfo)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(doctorsInfo).State = EntityState.Modified;
+                db.Entry(doctorsinfo).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(doctorsInfo);
+            ViewBag.DepartmentId = new SelectList(db.Departments, "Id", "DepartmentName", doctorsinfo.DepartmentId);
+            ViewBag.HospitalId = new SelectList(db.Hospitals, "Id", "HospitalName", doctorsinfo.HospitalId);
+            return View(doctorsinfo);
         }
 
-        // GET: DoctorsInfoes/Delete/5
-        [Authorize]
+        // GET: /DoctorsInfoes/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            DoctorsInfo doctorsInfo = db.DoctorsInfos.Find(id);
-            if (doctorsInfo == null)
+            DoctorsInfo doctorsinfo = db.DoctorsInfos.Find(id);
+            if (doctorsinfo == null)
             {
                 return HttpNotFound();
             }
-            return View(doctorsInfo);
+            return View(doctorsinfo);
         }
 
-        // POST: DoctorsInfoes/Delete/5
+        // POST: /DoctorsInfoes/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            DoctorsInfo doctorsInfo = db.DoctorsInfos.Find(id);
-            db.DoctorsInfos.Remove(doctorsInfo);
+            DoctorsInfo doctorsinfo = db.DoctorsInfos.Find(id);
+            db.DoctorsInfos.Remove(doctorsinfo);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
--- DoctorsOnline/Models/DoctorsInfo.cs
+++ DoctorsOnline/Models/DoctorsInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +19,17 @@ namespace Model
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:H:mm}")]
         [Display(Name="Visit Start")]
         public DateTime VisitStartTime { get; set; }
+        
         [DataType(DataType.Time)]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:H:mm}")]
         [Display(Name="Visit End")]
         public DateTime VisitEndTime { get; set; }
+
+        public int HospitalId { get; set; }
+        [ForeignKey("HospitalId")]
         public virtual Hospital Hospital { get; set; }
+        public int DepartmentId { get; set; }
+        [ForeignKey("DepartmentId")]
         public virtual Department Department { get; set; }
         public virtual ICollection<Appointment> Appointments { get; set; }
 
