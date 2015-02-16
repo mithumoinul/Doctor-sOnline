a4571343e58130aeeb7827ea227b9a8abcc9e537
ifta <ifta_005@yahoo.com>
2015-02-16 15:52:25 +0600

    AutomaticMigrationDataLossAllowed

--- DoctorsOnline/Migrations/Configuration.cs
+++ DoctorsOnline/Migrations/Configuration.cs
@@ -10,6 +10,7 @@ namespace DoctorsOnline.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(DoctorsOnline.Models.DoctorsOnlineContext context)
