f8ad918d1982b39cc5e69c68ab21d1004f2172c4
moinulmithu <moinulmithu@hotmail.com>
2015-02-18 22:00:57 +0600

    location

---
+++ DoctorsOnline/Migrations/201502181600076_location1.cs
@@ -0,0 +1,16 @@
+namespace DoctorsOnline.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class location1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
